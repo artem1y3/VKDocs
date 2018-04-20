7a3db91494e97da355c083f437e6c74a8d1983b8
artem1y3 <artem1y@outlook.com>
2018-04-20 04:26:44 +0300

    Parse JSON of docs

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-
-
+import vk
 
 
 def main():
-    
+    session = vk.Session(access_token='bc04953d549913e2e3f6888152e6e4e9a1c7b1048bbc66831e720f8b8e0a21801edd52acd36dc3f8a4cdc')
+    api = vk.API(session,v = 5.74)
+    # print(api.users.get(user_ids=1))
+    docs = api.docs.search(q='site.psd', count=5)
+    print(docs)
+    i = 0
+    for doc in docs['items']:
+        for key,att in doc.items():
+            print(key,att)
+        print('===========', i)
+        i += 1
 
 
-
-
-def __name__ = "__main__":
+if __name__ == "__main__":
     main()
